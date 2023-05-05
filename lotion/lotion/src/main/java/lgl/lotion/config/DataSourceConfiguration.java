71dd2de6eb092e31150a90b102c9bb33263bb18b
yujae-lee <yujjae.lee@gmail.com>
2023-05-06 01:53:57 +0900

    아직 mybatis 연동중ㅠ

---
+++ lotion/lotion/src/main/java/lgl/lotion/config/DataSourceConfiguration.java
@@ -0,0 +1,8 @@
+package lgl.lotion.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+class LotionDataSourceConfiguration {
+
+}
---
+++ lotion/lotion/src/main/java/lgl/lotion/mapper/UserMapper.java
@@ -0,0 +1,8 @@
+package lgl.lotion.mapper;
+
+import java.util.List;
+import java.util.Map;
+
+public interface UserMapper {
+    public List<Map<String, Object>> getUserMaster();
+}
